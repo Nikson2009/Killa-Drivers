6310efb69db2e9f92af5c3ff860c2d8645270152
misham228 <misham11x2009@gmail.com>
2024-11-22 19:32:11 +0300

    -

--- Assets/Scripts/MainMenu/MainMenuButtons.cs
+++ Assets/Scripts/MainMenu/MainMenuButtons.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 }
