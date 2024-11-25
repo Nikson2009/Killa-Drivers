1e386a3e29b749fd9ce92126b373307f4e187f02
Nickolay <nikson.aim@gmail.com>
2024-11-25 12:48:04 +0300

    One Restored

--- Assets/Scripts/Misc/CheckObjectsInRadiusWithVFX.cs
+++ Assets/Scripts/Misc/CheckObjectsInRadiusWithVFX.cs
@@ -28,20 +28,24 @@ public class CheckObjectsInRadiusWithVFX : MonoBehaviour
 
                     VFXCurveManager vfxCurveScript = newVfx.GetComponent<VFXCurveManager>();
 
-                    vfxCurveScript.SetCurvePoints(transform.position, new Vector3(), new Vector3(), checkingObject.transform.position);
+                    Vector3 centerPoint = transform.position + (checkingObject.transform.position - transform.position) / 2;
+
+                    newVfx.transform.position = centerPoint;
+
+                    vfxCurveScript.SetCurvePoints(transform.position, centerPoint, centerPoint, checkingObject.transform.position);
                     vfxCurveScript.StartTimerToDestroy();
 
                     returnedObjects.Add(checkingObject);
                 }
             }
 
-            //Destroy(gameObject);
+            Destroy(gameObject);
 
             return returnedObjects;
         }
         else
         {
-            //Destroy(gameObject);
+            Destroy(gameObject);
 
             return null;
         }
