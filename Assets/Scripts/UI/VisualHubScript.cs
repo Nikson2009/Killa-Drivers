45f4893b7d653748dce4d9bf2e7a42d67648be81
misham228 <misham11x2009@gmail.com>
2024-11-26 19:34:28 +0300

    -

--- Assets/Scripts/UI/VisualHubScript.cs
+++ Assets/Scripts/UI/VisualHubScript.cs
@@ -13,6 +13,9 @@ public class VisualHubScript : MonoBehaviour
 
     private void Update()
     {
-        Health_Text.text = PlayerScript.GetCurrentHealth() + "%";
+        //Health_Text.text = PlayerScript.GetCurrentHealth() + "%";
+        Color first = new Color(1, 1, 1);
+        Color second = new Color(1, 1, 1);
+        Health_Text.colorGradient = new VertexGradient(first,first, second,second);
     }
 }
