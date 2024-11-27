1743002f64e2294b930f511abcde23fa27d038bc
misham228 <misham11x2009@gmail.com>
2024-11-27 16:43:18 +0300

    -

--- Assets/Scripts/UI/VisualHubScript.cs
+++ Assets/Scripts/UI/VisualHubScript.cs
@@ -9,6 +9,8 @@ public class VisualHubScript : MonoBehaviour
     private List<int> MaxParams = new List<int>();
     [Header("Ui instances")]
     [SerializeField] TextMeshProUGUI Health_Text;
+    [SerializeField] TextMeshProUGUI Oxygen_Text;
+    [SerializeField] TextMeshProUGUI Stamina_Text;
     [Header("Other scripts instances")]
     [SerializeField] Player PlayerScript; //mb change to another with void damage received
     private void Start()
@@ -19,9 +21,16 @@ public class VisualHubScript : MonoBehaviour
     private void Update()
     {
         int curentHp = PlayerScript.GetCurrentHealth();
+        int curentOxygen = PlayerScript.GetCurrentOxygenLevel();
+        int curentStamina = PlayerScript.GetCurrentStamina();
+
         float curentHpInProc = curentHp / MaxParams[0];
-        print(curentHpInProc);
-        Health_Text.text = ((float)curentHp / (float)MaxParams[0])*100 + "%";
+        //Changing Variable text
+        Health_Text.text = curentHpInProc*100 + "%";
+        //Health_Text.text = curentHpInProc * 100 + "%";
+        //Health_Text.text = curentHpInProc * 100 + "%";
+
+        //Creating Gradient
         Color first = new Color((float)(MaxParams[0] - curentHp) / 100, 1, 1);
         Color second = new Color((float)(MaxParams[0] - curentHp) / MaxParams[0], curentHpInProc, curentHpInProc);
         Health_Text.colorGradient = new VertexGradient(first,first, second,second);
