f244680e87085488aa89515dc47177882712d70e
misham228 <misham11x2009@gmail.com>
2024-11-28 15:42:33 +0300

    -

--- Assets/Scripts/UI/ButtonAnim.cs
+++ Assets/Scripts/UI/ButtonAnim.cs
@@ -5,11 +5,26 @@ using UnityEngine.UI;
 
 public class ButtonAnim : MonoBehaviour
 {
+    [SerializeField] Image Arrow1;
+    [SerializeField] Image Arrow2;
+    [SerializeField] Color Color1;
+    [SerializeField] Color Color2;
     [SerializeField] Animator _animator;
-    [SerializeField] GameObject Visual;
-    public void Animates(string Event)
+    public void PlayAnim2(string Event)
     {
-        _animator.StopPlayback();
         _animator.Play(Event);
+    }
+    public void PlayAnim(bool Visible)
+    {
+        if (Visible)
+        {
+            Arrow1.color = Color1;
+            Arrow2.color = Color1;
+        }
+        else
+        {
+            Arrow1.color = Color2;
+            Arrow2.color = Color2;
+        }
     }
 }
