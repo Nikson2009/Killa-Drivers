0e68b6c7be34c52b3de8af2c0f575bb2afde2ae1
misham228 <misham11x2009@gmail.com>
2024-11-28 16:55:49 +0300

    -

--- Assets/Scripts/UI/ButtonAnim.cs
+++ Assets/Scripts/UI/ButtonAnim.cs
@@ -4,13 +4,17 @@ using UnityEngine.EventSystems;
 
 public class ButtonAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] GameObject Arrow1;
+    [SerializeField] GameObject Arrows;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Arrow1.transform.DOScale(1f, 0.5f);
+        Arrows.transform.DOScale(1f, 0.5f);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        Arrow1.transform.DOScale(0f, 0.5f);
+        Arrows.transform.DOScale(0f, 0.5f);
+    }
+    public void KillAnimation()
+    {
+        Arrows.transform.DOKill();
     }
 }
