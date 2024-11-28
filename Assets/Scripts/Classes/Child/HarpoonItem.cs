6973af238cc29570076d54c02f77cd5a3e7437ac
Nickolay <nikson.aim@gmail.com>
2024-11-28 15:42:10 +0300

    Harpoon At Half Completed

---
+++ Assets/Scripts/Classes/Child/HarpoonItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarpoonItem : WeaponItemClass
+{
+    [Header("Links")]
+    [SerializeField] GameObject harpoonLink;
+
+    [Header("Parameters")]
+    [SerializeField] float maxDistanceToHit = 500f;
+    [SerializeField] int maxDamagedEnemies = 3;
+    [SerializeField] float spawnDistance = 1.5f;
+    [SerializeField] float spawnForce = 725f;
+    [SerializeField] int damage = 5;
+    [SerializeField] int damageRandomness = 2;
+    public override void UseWeapon(Transform viewTransform)
+    {
+
+        Vector3 rayOrigin = viewTransform.position;
+        Vector3 rayDirection = viewTransform.forward;
+
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin, rayDirection, out hit, maxDistanceToHit))
+        {
+            GameObject weaponResult = Instantiate(harpoonLink, viewTransform.position + viewTransform.forward * spawnDistance, Quaternion.identity);
+            weaponResult.GetComponent<Rigidbody>().AddForce(viewTransform.forward * spawnForce, ForceMode.Force);
+            weaponResult.transform.rotation = viewTransform.rotation * Quaternion.Euler(0, 180f, 0);
+        }
+    }
+}
