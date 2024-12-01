6decdb6f7f6ba9b77bf5ba4b457777cd13bf1334
misham228 <misham11x2009@gmail.com>
2024-12-01 18:52:18 +0300

    Add grenade launcher

--- Assets/Scripts/DamageTypes/ExplosionDamageOnCollision.cs
+++ Assets/Scripts/DamageTypes/ExplosionDamageOnCollision.cs
@@ -12,43 +12,59 @@ public class ExplosionDamageOnCollision : MonoBehaviour
     [SerializeField] GameObject floatingTextLink;
 
     [Header("Parameters")]
-    [SerializeField] float timer;
     [SerializeField] float force;
+    [SerializeField] float timer;
+    private int damage;
+    private int damageRandomness;
 
-    public void StartExplosion(int damage, int damageRandomness)
+    private void Start()
     {
-        StartCoroutine(Explosion(damage, damageRandomness));
+        StartCoroutine(timerRealize(timer));
     }
-
-    IEnumerator Explosion(int damage, int damageRandomness)
+    public void StartExplosion(int damagee, int damageRandomnesss)
     {
-        yield return new WaitForSeconds(timer);
-        AudioS.Play();
-        gameObject.transform.localScale = new Vector3(0, 0, 0);
-        Destroy(gameObject,1);
-
-        Instantiate(explosionVFXLink, transform.position, Quaternion.identity);
-
-        GameObject newObjectChecker = Instantiate(objectCheckerLink, transform.position, Quaternion.identity);
-
-        CheckObjectsInRadius objectCheckerScript = newObjectChecker.GetComponent<CheckObjectsInRadius>();
-
-        List<GameObject> damagedEnemies = objectCheckerScript.getObjectsInRadius(100, 0, null);
-
-        foreach (GameObject enemy in damagedEnemies)
+        damage = damagee;
+        damageRandomness = damageRandomnesss;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Player")
         {
-            if (enemy.tag == "Enemy" || enemy.tag == "Player")
-            {
-                Entity entityScript = enemy.GetComponent<Entity>();
-                int damageResult = damage + Random.Range(-damageRandomness, damageRandomness);
-                entityScript.ApplyDamage(damageResult);
+            AudioS.Play();
+            gameObject.transform.localScale = new Vector3(0, 0, 0);
+            Destroy(gameObject, 1);
 
-                if (enemy.tag != "Player")
+            Instantiate(explosionVFXLink, transform.position, Quaternion.identity);
+
+            GameObject newObjectChecker = Instantiate(objectCheckerLink, transform.position, Quaternion.identity);
+
+            CheckObjectsInRadius objectCheckerScript = newObjectChecker.GetComponent<CheckObjectsInRadius>();
+
+            List<GameObject> damagedEnemies = objectCheckerScript.getObjectsInRadius(100, 0, null);
+
+            foreach (GameObject enemy in damagedEnemies)
+            {
+                if (enemy.tag == "Enemy" || enemy.tag == "Player")
                 {
-                    Rigidbody enemyRigidbody = enemy.GetComponent<Rigidbody>();
-                    enemyRigidbody.AddForce((enemy.transform.position - transform.position).normalized * force, ForceMode.Force);
+                    Entity entityScript = enemy.GetComponent<Entity>();
+                    int damageResult = damage + Random.Range(-damageRandomness, damageRandomness);
+                    entityScript.ApplyDamage(damageResult);
+
+                    if (enemy.tag != "Player")
+                    {
+                        Rigidbody enemyRigidbody = enemy.GetComponent<Rigidbody>();
+                        enemyRigidbody.AddForce((enemy.transform.position - transform.position).normalized * force, ForceMode.Force);
+                    }
                 }
             }
+        }
+    }
+    IEnumerator timerRealize(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        if(gameObject != null)
+        {
+            Destroy(gameObject);
         }
     }
 }
