dc5ddd66ff5b45678921a583f070cb19dab965ed
Nickolay <nikson.aim@gmail.com>
2024-12-02 08:34:20 +0300

    Weapon Icons

--- Assets/Scripts/Misc/GrabItem.cs
+++ Assets/Scripts/Misc/GrabItem.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrabItem : MonoBehaviour
 {
     [SerializeField] GameObject weaponItemLink;
+    [SerializeField] Sprite icon;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -15,7 +17,7 @@ public class GrabItem : MonoBehaviour
             GameObject newWeapon = Instantiate(weaponItemLink, Vector3.zero, Quaternion.identity);
             newWeapon.transform.parent = collision.gameObject.transform;
 
-            if (playerInventoryScript.TryAddWeapon(newWeapon))
+            if (playerInventoryScript.TryAddWeapon(newWeapon, icon))
             {
                 Destroy(transform.gameObject);
             }
--- Assets/Scripts/Player/InventoryManager.cs
+++ Assets/Scripts/Player/InventoryManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
@@ -16,21 +17,36 @@ public class InventoryManager : MonoBehaviour
     [SerializeField] Player playerScript;
     [SerializeField] float attackCooldown;
 
+    [SerializeField] GameObject[] inventoryBg;
+    [SerializeField] GameObject[] inventoryIcons;
+
     bool isAttacked = false;
 
     int currentWeapon = 0;
 
-    public bool TryAddWeapon(GameObject weapon)
+    public bool TryAddWeapon(GameObject weapon, Sprite icon)
     {
-        if (inventoryItems[1] == null)
+        if (inventoryItems[0] == null)
+        {
+            inventoryItems[0] = weapon;
+            inventoryIcons[0].GetComponent<Image>().sprite = icon;
+            inventoryIcons[0].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+            return true;
+        }
+        else if (inventoryItems[1] == null)
         {
             inventoryItems[1] = weapon;
+            inventoryIcons[1].GetComponent<Image>().sprite = icon;
+            inventoryIcons[1].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
 
             return true;
         }
         else if (inventoryItems[2] == null)
         {
             inventoryItems[2] = weapon;
+            inventoryIcons[2].GetComponent<Image>().sprite = icon;
+            inventoryIcons[2].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
 
             return true;
         }
@@ -69,11 +85,39 @@ public class InventoryManager : MonoBehaviour
         {
             currentWeapon += 1;
             currentWeapon = Mathf.Clamp(currentWeapon, 0, 2);
+
+            int i = 0;
+            foreach (GameObject bg in inventoryBg)
+            {
+
+                Image bgImage = bg.GetComponent<Image>();
+                bgImage.color = new Color32(145, 145, 145, 220);
+                if (i == currentWeapon)
+                {
+                    bgImage.color = new Color32(105, 105, 105, 220);
+                }
+
+                i += 1;
+            }
         }
         if (Input.GetKeyDown(KeyCode.Q))
         {
             currentWeapon -= 1;
             currentWeapon = Mathf.Clamp(currentWeapon, 0, 2);
+
+            int i = 0;
+            foreach (GameObject bg in inventoryBg)
+            {
+
+                Image bgImage = bg.GetComponent<Image>();
+                bgImage.color = new Color32(145, 145, 145, 220);
+                if (i == currentWeapon)
+                {
+                    bgImage.color = new Color32(105, 105, 105, 220);
+                }
+
+                i += 1;
+            }
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
@@ -84,6 +128,9 @@ public class InventoryManager : MonoBehaviour
                 GameObject newItemToGrab = Instantiate(currentWeaponScript.GetItemToGrab(), playerCamera.transform.position + playerCamera.transform.forward * 1.5f, Quaternion.identity);
                 Rigidbody newItemToGrabRb = newItemToGrab.GetComponent<Rigidbody>();
                 newItemToGrabRb.AddForce(playerCamera.transform.forward * 50f, ForceMode.Force);
+
+                inventoryIcons[currentWeapon].GetComponent<Image>().sprite = null;
+                inventoryIcons[currentWeapon].GetComponent<Image>().color = new Color32(0, 0, 0, 0);
 
                 Destroy(inventoryItems[currentWeapon]);
                 inventoryItems[currentWeapon] = null;
